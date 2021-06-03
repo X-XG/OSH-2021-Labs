de3f6f96111fef515996e528f269a2635bf28cde
X-XG <9341624962@qq.com>
2021-06-03 20:41:05 +0800

    complete some additions

--- lab2/src/main.rs
+++ lab2/src/main.rs
@@ -1,12 +1,14 @@
-use std::io::{stdin,stdout, BufRead, Write, Read};
+use core::option::Option;
+use std::collections::VecDeque;
 use std::env;
+use std::fs::OpenOptions;
+use std::io::{stdin, stdout, BufRead, Read, Write};
+use std::net::TcpStream;
+use std::process;
 use std::process::{exit, Command, Stdio};
+use std::string::String;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
-use core::option::Option;
-use std::process;
-use std::fs::OpenOptions;
-use std::string::String;
 
 extern crate ctrlc;
 
@@ -21,6 +23,7 @@ fn main() -> ! {
     })
     .expect("Error setting Ctrl-C handler");
 
+    let mut seted = false;
     loop {
         if ruptc.load(Ordering::SeqCst) {
             //情况二:终结运行中程序
@@ -32,54 +35,71 @@ fn main() -> ! {
 
         //输入
         let mut cmd = String::new();
-        stdin().lock().read_line(&mut cmd).expect("Read a line from stdin failed");
-        
-        if cmd.is_empty() {     //Ctrl+D 退出shell
+        stdin()
+            .lock()
+            .read_line(&mut cmd)
+            .expect("Read a line from stdin failed");
+
+        if cmd.is_empty() {
+            //Ctrl+D 退出shell
             println!("");
             exit(0);
-        }    
+        }
 
         if ruptc.load(Ordering::SeqCst) {
             //情况一:丢弃未输完命令
             ruptc.store(false, Ordering::SeqCst);
         }
-        
         let mut result = String::new();
-        let last_child_thread = excute(None, &cmd, true);
-        if last_child_thread.is_none() == true {continue;}
-        last_child_thread.expect("excute failed")
+
+        let ex_result = excute(None, &cmd, true, seted);
+        let last_child_thread = ex_result.0;
+        seted = ex_result.1;
+
+        if last_child_thread.is_none() == true {
+            continue;
+        }
+
+        last_child_thread
+            .expect("excute failed")
             .read_to_string(&mut result)
             .expect("print final result failed");
-        print!("{}",result);
-        stdout().flush().expect("Print result failed");
+
+        if ex_result.2 {
+            let mut stream = TcpStream::connect(ex_result.3).expect("connect failed");
+            stream.write(result.as_bytes()).expect("write tcp failed");
+        } else {
+            print!("{}", result);
+            stdout().flush().expect("Print result failed");
+        }
     }
 }
 
-fn find_first(string: &str) -> (&str,i32,&str) {
+fn find_first(string: &str) -> (&str, i32, &str) {
     //用于拆分"<",">>",">",并得到第一处的拆分
-    let mut retype=0;
-    let mut re_find=true;
-    let mut temp = string;
+    let mut retype = 0;
+    let mut re_find = true;
+    let mut fore = string;
     let mut behind = "";
-    while re_find { 
-        re_find=true;
-        if temp.find("<").is_some() {
-            temp = temp.split_once("<").unwrap().0;
+    while re_find {
+        re_find = true;
+        if fore.find("<").is_some() {
+            fore = fore.split_once("<").unwrap().0;
             behind = string.split_once("<").unwrap().1;
-            retype=1;
-        } else if temp.find(">>").is_some() {
-            temp = temp.split_once(">>").unwrap().0;
+            retype = 1;
+        } else if fore.find(">>").is_some() {
+            fore = fore.split_once(">>").unwrap().0;
             behind = string.split_once(">>").unwrap().1;
-            retype=2;
-        } else if temp.find(">").is_some() {
-            temp = temp.split_once(">").unwrap().0;
+            retype = 2;
+        } else if fore.find(">").is_some() {
+            fore = fore.split_once(">").unwrap().0;
             behind = string.split_once(">").unwrap().1;
-            retype=3;
+            retype = 3;
         } else {
             re_find = false;
         }
     }
-    (temp,retype,behind)
+    (fore, retype, behind)
 }
 
 //递归调用实现多层pipe,在各个线程内使用管道,与主线程形成环
@@ -87,7 +107,9 @@ fn excute(
     pipein: Option<process::ChildStdout>,
     cmd: &str,
     isfirst: bool,
-) -> Option<process::ChildStdout> {
+    seted: bool,
+) -> (Option<process::ChildStdout>, bool, bool, String) {
+
     let yy = cmd.split_once('|');
 
     let mut output: Option<process::ChildStdout> = None;
@@ -106,44 +128,130 @@ fn excute(
         Stdio::from(pipein.expect("pipe with no input"))
     };
 
-    let mut stdout=Stdio::piped();
+    let mut stdout = Stdio::piped();
 
+    let mut istcp;
     let mut redirect = true;
-    //let mut is_fread = false;
+    let mut tcp_write = false;
+    let mut tcp_read = false;
+    let mut tcp = String::new();
+    let mut tcpread = String::new();
+
     while redirect {
         //处理重定向
         let head = find_first(cmdcurrent);
+        //let fore = head.0.trim();
         let filename = find_first(head.2).0.trim();
-        //println!("0: {}\n{}\n\n\n",filename,cmdcurrent);
+        if filename.find("/dev/tcp/").is_some() {
+            istcp = true;
+            tcp = filename.replace("/dev/tcp/", "");
+            tcp = tcp.replacen("/", ":", 1);
+        } else {
+            istcp = false;
+        }
+
         match head.1 {
             1 => {
                 cmdcurrent = head.2;
-                let fread = OpenOptions::new().read(true).open(filename).expect("there is no such file");
-                stdin=Stdio::from(fread);
-                //is_fread = true;
+                if istcp {
+                    tcp_read = true;
+                    let mut stream = TcpStream::connect(&tcp).expect("connect failed");
+                    stream
+                        .read_to_string(&mut tcpread)
+                        .expect("read from tcp failed");
+                } else {
+                    let fread = OpenOptions::new()
+                        .read(true)
+                        .open(filename)
+                        .expect("there is no such file");
+                    stdin = Stdio::from(fread);
+                }
             }
             2 => {
                 cmdcurrent = head.2;
-                let fwrite = OpenOptions::new().write(true).append(true).create(true).open(filename).unwrap();
-                stdout=Stdio::from(fwrite); 
-                //println!("2: {}\n{}\n",filename,cmdcurrent);
+                if istcp {
+                    tcp_write = true;
+                } else {
+                    let fwrite = OpenOptions::new()
+                        .write(true)
+                        .append(true)
+                        .create(true)
+                        .open(filename)
+                        .unwrap();
+                    stdout = Stdio::from(fwrite);
+                }
             }
             3 => {
                 cmdcurrent = head.2;
-                let fwrite = OpenOptions::new().write(true).truncate(true).create(true).open(filename).unwrap();
-                stdout=Stdio::from(fwrite);
-                //println!("3: {}\n{}\n",filename,cmdcurrent);
+                if istcp {
+                    tcp_write = true;
+                } else {
+                    let fwrite = OpenOptions::new()
+                        .write(true)
+                        .truncate(true)
+                        .create(true)
+                        .open(filename)
+                        .unwrap();
+                    stdout = Stdio::from(fwrite);
+                }
             }
-            _ => { redirect = false }
+            _ => redirect = false,
         }
     }
-    
-    let mut args = cmdpure.split_whitespace();
+
+    let mut v: VecDeque<&str> = cmdpure.split('$').collect();
+    v.pop_front();
+
+    let mut cmdchange = cmdpure.to_owned();
+    for ans in v {
+        //替换环境变量
+        match env::var(ans.trim()) {
+            Ok(val) => {
+                cmdchange = cmdchange.replacen("$", "", 1);
+                cmdchange = cmdchange.replacen(ans, &val, 1);
+            }
+            _ => {
+                if seted {
+                    panic!("{}: unbound variable", ans.trim());
+                }
+            }
+        }
+    }
+
+    //替换~
+    let mut v: VecDeque<&str> = cmdpure.split('$').collect();
+    v.pop_front();
+
+    let path = env::var("HOME").expect("No home varible");
+    let user = env::var("USER").expect("No user varible");
+    let mut temp = String::from("~");
+    temp.push_str(&user);
+    cmdchange = cmdchange.replace("~root", "/root");
+    cmdchange = cmdchange.replace(&temp, &path);
+
+    if cmdchange.find('~').is_some() {
+        let offset = cmdchange.find('~').unwrap();
+        println!("{:?}",cmd.get(offset-1..offset));
+        println!("{:?}",cmd.get(offset+1..offset+2));
+        if cmd.get(offset-1..offset) == Some(&" ") &&
+           ( cmd.get(offset+1..offset+2) == Some(&"\n") || 
+             cmd.get(offset+1..offset+2) == Some(&" ") ) {
+            cmdchange.replace_range(offset..offset+1, &path)
+        }
+    }
+
+    let mut nextset = false;
+    let mut args = cmdchange.split_whitespace();
     let prog = args.next();
     match prog {
         None => panic!("Not program input"),
         Some(prog) => {
             match prog {
+                "set" => {
+                    if args.next().expect("No enough args of set").trim() == "-u" {
+                        nextset = true;
+                    }
+                }
                 "cd" => {
                     let dir = args.next().expect("No enough args to set current dir");
                     env::set_current_dir(dir).expect("Changing current dir failed");
@@ -164,20 +272,31 @@ fn excute(
                     exit(0);
                 }
                 _ => {
-                    let child = Command::new(prog)
-                        .args(args)
-                        .stdin(stdin)
-                        .stdout(stdout)
-                        .spawn()
-                        .expect("Failed to execute command");
+                    let child = if tcp_read {
+                        Command::new(prog)
+                            .args(args)
+                            .arg(tcpread)
+                            .stdin(Stdio::null())
+                            .stdout(stdout)
+                            .spawn()
+                            .expect("Failed to execute command")
+                    } else {
+                        Command::new(prog)
+                            .args(args)
+                            .stdin(stdin)
+                            .stdout(stdout)
+                            .spawn()
+                            .expect("Failed to execute command")
+                    };
                     output = child.stdout;
                 }
             }
         }
     }
+
     if yy == None {
-        output
+        (output, nextset, tcp_write, tcp)
     } else {
-        excute(output, cmdnext, false)
+        excute(output, cmdnext, false, nextset)
     }
 }

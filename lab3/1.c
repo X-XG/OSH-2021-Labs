6ce234965e29f13874bb7ac0c628057cbff2d93c
X-XG <934162496@qq.com>
2021-06-12 16:49:19 +0800

    temp 1.c

--- lab3/1.c
+++ lab3/1.c
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#define BUF 1000
 
 struct Pipe {
     int fd_send;
@@ -13,22 +14,37 @@ struct Pipe {
 
 void *handle_chat(void *data) {
     struct Pipe *pipe = (struct Pipe *)data;
-    char buffer[1024] = "Message:";
+    char msg[1050000]="Message:";
+    char buffer[BUF];
     ssize_t len;
+    int start = 8;
     while(1){
-        len = recv(pipe->fd_send, buffer + 8, 1000, 0);
+        //memset(buffer,'\0',1024*sizeof(char));
+        len = recv(pipe->fd_send, buffer, BUF-12, 0);
+        printf("the len is: %ld\n",len);
+        printf("the buffer is: %s////////////////////////",buffer);
         int i;
-        char temp[1024];
-        for (i=8;i<len+8;i++){
+        int sig = 0;
+        int num = 0;
+        for (i=0;i<len;i++){
             if(buffer[i]=='\n'){
-                strcpy(temp,buffer+i);
-                buffer[i]='\0';
-                break;
+                num = i -sig +1;
+                strncpy(msg+start,buffer+sig,num);
+                
+                //printf("the msg is:\n%s////////////\n",msg);
+                //printf("start+sig is: %d\n",start+sig);
+                send(pipe->fd_recv, msg, start+num, 0);
+                sig=i+1;
+                start =8;
             }
         }
-        /*while ((len = recv(pipe->fd_send, buffer + 8, 1000, 0)) > 0) {
-            send(pipe->fd_recv, buffer, len + 8, 0);
-        }*/
+        printf("sig is: %d\n",sig);
+        if(sig!=len){
+            num = len-sig;
+            strncpy(msg+start,buffer+sig,num);
+            start=start+num;
+            printf("the start is: %d\n",start);
+        }
     }
     return NULL;
 }
